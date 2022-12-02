debdc68a13283b9d65c9b295f1be28a40ded8adb
javier-botija <j.botija@uoc.edu>
2022-12-02 18:45:25 +0100

    All Data generation by subject

--- load.py
+++ load.py
@@ -5,6 +5,12 @@ import globals as gb
 import numpy as np
 
 
+def damefecha(mrow):
+    mfechastr = "01/01/" + str(mrow.Year)
+    mfecha = pd.to_datetime(mfechastr)
+    return mfecha
+
+
 def recorre_ficheros(directorio):
     global df_dest
     ficheros = os.listdir(directorio)
@@ -27,25 +33,29 @@ def importa_fichero(directorio, fich):
 
 def crea_tabla_resumen(fichgeo, fichind):
     #global df_dest
-    df_dest = pd.read_csv(gb.pathData + 'dataraw.csv', sep=';', decimal=',')
-    # Filtro de años
-    #df_dest = df_dest[df_dest['Year']==2021]
+    df_data = pd.read_csv(gb.pathData + 'dataraw.csv', sep=';', decimal=',')
+    df_data['ValueDate'] = df_data.apply(damefecha, axis=1)
+
     df_ind = pd.read_csv(fichind, sep=';')
 
-    df_dest = df_dest.merge(df_ind, left_on='Indicator', right_on='Indicador')
-    # Filtro de Subtema
-    df_dest = df_dest[df_dest['SubTema'] == 'Vivienda']
-    # Agrupamos los resultados
-    df_dest=df_dest.groupby(
-        ['DT','DTBA','Year', df_dest.Ind_Esp.values]
-    ).Value.sum().unstack().reset_index()
+    df_data = df_data.merge(df_ind, left_on='Indicator', right_on='Indicador')
 
     gdf_barrios = gpd.read_file(fichgeo)
     gdf_barrios.insert(0, "DTBA", pd.to_numeric(gdf_barrios.coddistbar, downcast='integer'), True)
-    gdf_final = gdf_barrios.merge(df_dest, on='DTBA')
-    gdf_final = gdf_final.drop(['gis_gis_barrios_area','objectid','linkid','codbarrio','coddistbar','last_edited_user','last_edited_date'], axis=1)
-    gdf_final.to_csv(gb.pathData + 'data.csv', sep=';', decimal='.', index=False)
-    gdf_final.to_file(gb.pathData + 'data.geojson', driver='GeoJSON')
+
+    # Filtro de Subtema
+    for Tema in df_data['Tema'].unique():
+        df_datast = df_data[df_data['Tema'] == Tema]
+        # Agrupamos los resultados
+        df_datast=df_datast.groupby(
+            ['DT','DTBA','ValueDate', df_datast.Ind_Esp.values]
+        ).Value.sum().unstack().reset_index()
+
+        gdf_final = gdf_barrios.merge(df_datast, on='DTBA')
+
+        gdf_final = gdf_final.drop(['gis_gis_barrios_area','objectid','linkid','codbarrio','coddistbar','last_edited_user','last_edited_date'], axis=1)
+        gdf_final.to_csv(gb.pathData + 'data_' + Tema + '.csv', sep=';', decimal='.', index=False)
+        #gdf_final.to_file(gb.pathData + 'data_' + Tema + '.geojson', driver='GeoJSON')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
